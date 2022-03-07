ab25861bccf529f8000334949905236b17b8f408
Igor Vieira <igorsv2240@gmail.com>
2022-03-07 18:20:08 -0300

    tarefa 1

---
+++ fundamentos/bloco_4_Introducao_a_JavaScript_e_Logica_de_Programação/dia_1_JavaScript - Primeiros passos/script.js
@@ -0,0 +1,13 @@
+const a = 56;
+const b = 4;
+
+let adicao = a + b;
+    console.log(adicao);
+let Subtração = a - b;
+    console.log(Subtração);
+let Multiplicação = a * b;
+    console.log(Multiplicação)
+let Divisão = a / b;
+    console.log(Divisão);
+let Módulo = a % b;
+    console.log(Módulo);                
