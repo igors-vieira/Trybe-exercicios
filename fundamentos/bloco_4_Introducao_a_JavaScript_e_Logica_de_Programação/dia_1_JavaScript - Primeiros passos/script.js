03d55bcbd45f1ee23b22b9f1623032946fe3b963
Igor Vieira <igorsv2240@gmail.com>
2022-03-07 18:28:17 -0300

    tarefa 2

--- fundamentos/bloco_4_Introducao_a_JavaScript_e_Logica_de_Programação/dia_1_JavaScript - Primeiros passos/script.js
+++ fundamentos/bloco_4_Introducao_a_JavaScript_e_Logica_de_Programação/dia_1_JavaScript - Primeiros passos/script.js
@@ -1,13 +1,8 @@
 const a = 56;
-const b = 4;
+const b = 100;
 
-let adicao = a + b;
-    console.log(adicao);
-let Subtração = a - b;
-    console.log(Subtração);
-let Multiplicação = a * b;
-    console.log(Multiplicação)
-let Divisão = a / b;
-    console.log(Divisão);
-let Módulo = a % b;
-    console.log(Módulo);                
+if (a > b) {
+    console.log(a)
+}else {
+    console.log(b)
+}
