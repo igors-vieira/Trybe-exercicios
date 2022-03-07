55ee29c63cfa241edae34f1de456f86f01c5ad43
Igor Vieira <igorsv2240@gmail.com>
2022-03-07 19:14:37 -0300

    tarefa 3

--- fundamentos/bloco_4_Introducao_a_JavaScript_e_Logica_de_Programação/dia_1_JavaScript - Primeiros passos/script.js
+++ fundamentos/bloco_4_Introducao_a_JavaScript_e_Logica_de_Programação/dia_1_JavaScript - Primeiros passos/script.js
@@ -1,8 +1,10 @@
-const a = 56;
-const b = 100;
-
-if (a > b) {
-    console.log(a)
-}else {
-    console.log(b)
+const num1 = 5644;
+const num2 = 100;
+const num3 = 777;
+if (num1 > num2 && num1 > num3) {
+  console.log(num1);
+} else if (num2 > num1 && num2 > num3 ) {
+  console.log(num2);
+} else {
+  console.log(num3);
 }
