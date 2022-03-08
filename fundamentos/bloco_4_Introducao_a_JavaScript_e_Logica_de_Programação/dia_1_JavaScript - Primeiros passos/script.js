f1caf41688baada3df380e75907fad4c0b629dde
Igor Vieira <igorsv2240@gmail.com>
2022-03-08 10:37:26 -0300

    tarefa 7

--- fundamentos/bloco_4_Introducao_a_JavaScript_e_Logica_de_Programação/dia_1_JavaScript - Primeiros passos/script.js
+++ fundamentos/bloco_4_Introducao_a_JavaScript_e_Logica_de_Programação/dia_1_JavaScript - Primeiros passos/script.js
@@ -1,8 +1,14 @@
-let pecaDeXadrex = "Bispo";
-pecaDeXadrex = pecaDeXadrex.toLowerCase();
-
-    if (pecaDeXadrex === "bispo") {
-        console.log("diagonais")
-    } else {
-        console.log("error")
-    }
+let porcentagem = null
+if (porcentagem >= 90) {
+    console.log("A")
+} else if (porcentagem >= 80) {
+    console.log("B")
+} else if (porcentagem >= 70) {
+    console.log("C")
+} else if (porcentagem >= 60) {
+    console.log("D")
+} else if (porcentagem >= 50) {
+    console.log("E")
+} else {
+    console.log("F")
+}
