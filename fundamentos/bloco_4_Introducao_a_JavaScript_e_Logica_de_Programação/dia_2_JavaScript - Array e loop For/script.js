db086f8842dba410e25fb148e0ce95b9451adb7e
Igor Vieira <igorsv2240@gmail.com>
2022-03-08 19:31:57 -0300

    tarefa 4

--- fundamentos/bloco_4_Introducao_a_JavaScript_e_Logica_de_Programação/dia_2_JavaScript - Array e loop For/script.js
+++ fundamentos/bloco_4_Introducao_a_JavaScript_e_Logica_de_Programação/dia_2_JavaScript - Array e loop For/script.js
@@ -51,4 +51,8 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
     for (let index = 0; index < numbers.length; index += 1) {
         soma += numbers[index]
         media = soma / numbers.length  
-    }if 
+    }if (media > 20){
+        console.log("valor maior que 20")
+    }else {
+        console.log("valor menor ou igual a 20")
+    }
