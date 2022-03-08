ca739478446a1691ec6b4bc92f71d2e2f437422b
Igor Vieira <igorsv2240@gmail.com>
2022-03-08 18:06:23 -0300

    tarefa 1

---
+++ fundamentos/bloco_4_Introducao_a_JavaScript_e_Logica_de_Programação/dia_2_JavaScript - Array e loop For/script.js
@@ -0,0 +1,29 @@
+// let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+// let menuServices = menu[1];
+
+// console.log(menuServices);
+
+// let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+// let indexOfPortfolio = menu[2];
+
+// console.log(indexOfPortfolio);
+
+// let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+// menu.push("contatos")
+// console.log(menu);
+
+// let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+//     for(let i = 0; i < groceryList.length; i += 1) {
+//         console.log(groceryList[i]) 
+//     }
+
+// let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+//     for (const array of names) {
+//         console.log(array)
+//     }
+
+// TAREFA 1
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+    for (const number of numbers) {
+        
+    }console.log(numbers)
