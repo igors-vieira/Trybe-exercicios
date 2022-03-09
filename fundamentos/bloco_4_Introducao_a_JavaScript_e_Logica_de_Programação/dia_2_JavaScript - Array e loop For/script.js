2b967dac0621d917ba5e58e3cc6ac5b2e58bd675
Igor Vieira <igorsv2240@gmail.com>
2022-03-09 12:56:17 -0300

    tarefa 5

--- fundamentos/bloco_4_Introducao_a_JavaScript_e_Logica_de_Programação/dia_2_JavaScript - Array e loop For/script.js
+++ fundamentos/bloco_4_Introducao_a_JavaScript_e_Logica_de_Programação/dia_2_JavaScript - Array e loop For/script.js
@@ -1,3 +1,4 @@
+/////EXERCICIO DO ESTUDO
 // let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
 // let menuServices = menu[1];
 
@@ -14,45 +15,56 @@
 
 // let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
 //     for(let i = 0; i < groceryList.length; i += 1) {
-//         console.log(groceryList[i]) 
+//         console.log(groceryList[i])
 //     }
 
 // let names = ['João', 'Maria', 'Antônio', 'Margarida'];
 //     for (const array of names) {
 //         console.log(array)
 //     }
+/////EXERCICIO DO ESTUDO
 
 // TAREFA 1
 // let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 //     for (const number of numbers) {
-        
+
 //     }console.log(numbers)
 
 // TAREFA 2
 // let soma = 0;
 // let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 //     for (let index = 0; index < numbers.length; index += 1) {
-//         soma += numbers[index]  
+//         soma += numbers[index]
 //     }console.log(soma)
 
-// TAREFA 3 
+// TAREFA 3
 // let media = 0;
 // let soma = 0;
 // let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 //     for (let index = 0; index < numbers.length; index += 1) {
 //         soma += numbers[index]
-//         media = soma / numbers.length  
+//         media = soma / numbers.length
 //     }console.log(media)
 
 // TAREFA 4
-let divisao = 0;
-let soma = 0;
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-    for (let index = 0; index < numbers.length; index += 1) {
-        soma += numbers[index]
-        media = soma / numbers.length  
-    }if (media > 20){
-        console.log("valor maior que 20")
-    }else {
-        console.log("valor menor ou igual a 20")
-    }
+// let divisao = 0;
+// let soma = 0;
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+//     for (let index = 0; index < numbers.length; index += 1) {
+//         soma += numbers[index]
+//         media = soma / numbers.length
+//     }if (media > 20){
+//         console.log("valor maior que 20")
+//     }else {
+//         console.log("valor menor ou igual a 20")
+//     }
+
+// TAREFA 5
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let maior = 0;
+// for (let index = 0; index < numbers.length; index += 1) {
+//   if (numbers[index] > maior) {
+//     maior = numbers[index];
+//   }
+// }
+// console.log(maior);
