3527f2aff67ff8d806c8f6cd492fe2d7f551b73c
Igor Vieira <igorsv2240@gmail.com>
2022-03-09 16:09:48 -0300

    tarefa 6

--- fundamentos/bloco_4_Introducao_a_JavaScript_e_Logica_de_Programação/dia_2_JavaScript - Array e loop For/script.js
+++ fundamentos/bloco_4_Introducao_a_JavaScript_e_Logica_de_Programação/dia_2_JavaScript - Array e loop For/script.js
@@ -68,3 +68,16 @@
 //   }
 // }
 // console.log(maior);
+
+// TAREFA 6
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let impares = 0;
+for (let index = 0; index < numbers.length; index += 1) {
+  if (numbers[index] % 2 !== 0) {
+    impares += 1;
+  }
+}
+if (impares === 0) {
+  console.log("nenhum valor ímpar encontrado");
+}
+console.log(impares);
