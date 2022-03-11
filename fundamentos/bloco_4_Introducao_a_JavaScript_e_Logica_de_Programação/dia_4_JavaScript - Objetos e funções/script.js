0353a471c2f047278e458ee9e3f7f2a922f61a49
Igor Vieira <igorsv2240@gmail.com>
2022-03-11 12:15:33 -0300

    exercicios dia 4

---
+++ fundamentos/bloco_4_Introducao_a_JavaScript_e_Logica_de_Programação/dia_4_JavaScript - Objetos e funções/script.js
@@ -0,0 +1,138 @@
+// let names = {
+//     person1: 'João',
+//     person2: 'Maria',
+//     person3: 'Jorge',
+// };
+// for ( let key in names){
+//     console.log("óla " + names[key])
+// }
+
+// let car = {
+//     model: 'A3 Sedan',
+//     manufacturer: 'Audi',
+//     year: 2020
+//   };
+//   for (const key in car) {
+//     console.log(key +" "+ car[key])  
+//   }
+
+// TAREFA 1
+// let info = {
+//   personagem: 'Margarida',
+//   origem: 'Pato Donald',
+//   nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+// };
+// for (const key in info) {
+
+// }console.log('bem vindo' + ',' + ' ' + info['personagem'])
+
+// TAREFA 2
+// let info = {
+//   personagem: 'Margarida',
+//   origem: 'Pato Donald',
+//   nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+//   recorrente: 'Sim',
+// };
+// for (const key in info){
+  
+// }console.log(info.recorrente)
+
+// TAREFA 3 
+// let info = {
+//   personagem: 'Margarida',
+//   origem: 'Pato Donald',
+//   nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+//   recorrente: 'Sim',
+// };
+// for (const key in info){
+//     console.log(key)
+// }
+
+// TAREFA 4 
+// let info = {
+//   personagem: 'Margarida',
+//   origem: 'Pato Donald',
+//   nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+//   recorrente: 'Sim',
+// };
+// for (const key in info){
+//     console.log(info[key])
+// }
+
+// TAREFA 5
+// let info = {
+//     personagem: 'Margarida' + 'Tio Patinhas',
+//     origem: 'Pato Donald' + "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+//     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald e ' + 'O último MacPatinhas',
+//     recorrente: 'Sim'+ 'Sim',
+//   }
+//   for (const key in info){
+//       console.log(info[key])
+//   }
+
+// TAREFA 6
+// let leitor = {
+//   nome: 'Julia',
+//   sobrenome: 'Pessoa',
+//   idade: 21,
+//   livrosFavoritos: [
+//     {
+//       titulo: 'O Pior Dia de Todos',
+//       autor: 'Daniela Kopsch',
+//       editora: 'Tordesilhas',
+//     },
+//   ],
+// };
+// for (const key in leitor){
+//   
+// }console.log('O livro favorito de ' + '' + leitor['nome'] + ' ' + leitor['sobrenome'] + ' ' + "se chama " + leitor.livrosFavoritos[0].titulo + '"') ;
+
+// TAREFA 7 
+// let leitor = {
+//   nome: 'Julia',
+//   sobrenome: 'Pessoa',
+//   idade: 21,
+//   livrosFavoritos: [
+//     {
+//       titulo: 'O Pior Dia de Todos',
+//       autor: 'Daniela Kopsch',
+//       editora: 'Tordesilhas',
+//     },
+//   ],
+// };leitor.livrosFavoritos[1] = 
+//   {
+//   titulo: 'Harry Potter e o Prisioneiro de Azkaban',
+//   autor: 'JK Rowling',
+//   editora: 'Rocco',
+// },
+// console.log(leitor)
+
+// TAREFA 8
+let leitor = {
+  nome: 'Julia',
+  sobrenome: 'Pessoa',
+  idade: 21,
+  livrosFavoritos: [
+    {
+      titulo: 'O Pior Dia de Todos',
+      autor: 'Daniela Kopsch',
+      editora: 'Tordesilhas',
+    },
+  ],
+};leitor.livrosFavoritos[1] =
+  {
+  titulo: 'Harry Potter e o Prisioneiro de Azkaban',
+  autor: 'JK Rowling',
+  editora: 'Rocco',
+}
+for (const key in leitor) {
+  if (leitor.livrosFavoritos[] === leitor.livrosFavoritos[]) {
+     console.log(leitor.nome +" "+"tem 2" + ' ' + leitor[key])
+  }
+}
+
+
+
+  
+
+
