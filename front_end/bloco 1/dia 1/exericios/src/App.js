eb6e709dc6d9ebc51d4c5cef41a7354199f75742
Igor Vieira <igorsv2240@gmail.com>
2022-05-05 18:58:09 -0300

    iniciando Front-End

---
+++ front_end/bloco 1/dia 1/exericios/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+const Task = ({ value }) => {
+  return (
+    <li>{value}</li>
+  );
+}
+
+function App() {
+  return (
+    <div className="App">
+      hello, world!
+      <Task value={'abc'}/>
+    </div>
+  );
+}
+
+export default App;
