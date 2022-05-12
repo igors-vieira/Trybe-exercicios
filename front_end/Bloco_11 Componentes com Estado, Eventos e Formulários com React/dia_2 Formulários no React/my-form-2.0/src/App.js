b200924056e1076abf2ccf499ec6ea54caa2ab82
Igor Vieira <igorsv2240@gmail.com>
2022-05-12 09:51:30 -0300

    exercicio/11.2, 1 - feito

--- front_end/Bloco_11 Componentes com Estado, Eventos e Formulários com React/dia_2 Formulários no React/my-form-2.0/src/App.js
+++ front_end/Bloco_11 Componentes com Estado, Eventos e Formulários com React/dia_2 Formulários no React/my-form-2.0/src/App.js
@@ -2,12 +2,23 @@ import React from 'react'
 import './App.css';
 
 class App extends React.Component {
+
+  state = {
+    name: '',
+  }
+
+  handleChange = ({ target }) => {
+    const { name, value } = target
+    this.setState({ [name]: value })
+    
+  }
+
   render() {
     return (
       <fieldset>
         <form>
           <label>Name
-            <input type="text" name="name"/>        
+            <input type="text" onChange={this.handleChange} name="name" value={this.state.name} maxLength='40'/>        
           </label>
         </form>
       </fieldset>
